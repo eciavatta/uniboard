c28b4705f7275a4cf09f84047da6b953c3dcb348
Passw0rd! <segreto@nontelodico>
2019-07-05 18:43:55 +0200

    Added courses and activities to db

---
+++ src/models/coursesModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CoursesSchema = new Schema({
+  name: {
+    type: String,
+    required: "Course name is required"
+  },
+  teachers: {
+    type: [String],
+    required: "A course should have at least a teacher",
+    validate: {
+      validator: function(v) {
+        return v.length > 0
+      },
+      message: "A course should have at least a teacher"
+    }
+  },
+  tutors: {
+    type: [String]
+  },
+  degree: {
+    type: String,
+    required: "A course should belong to a degree"
+  },
+  year: {
+    type: Number,
+    required: "A course should have a year"
+  }
+});
+
+module.exports = mongoose.model('Course', CoursesSchema);
